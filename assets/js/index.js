a0986815df77909d0fcb1f88487c0ca33a158e33
Noel Quiles <noelquilesgraphics@gmail.com>
2016-06-06 16:56:15 -0400

    VueJS integration I: Baby Steps
    Probably running into an issue with how I imported the components into index.js, will look into it for II

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,29 @@
-const $ = require('jquery')
+// include libs
+// const $ = require('jquery')
 const animateCss = require('animate.css-js')
 const Vue = require('vue')
-// const blast = require('blast-text')
+// const letterify = require('letterify')
 
+// include Vue single-file components
+const frontends = require('../../components/frontends.vue')
+const games = require('../../components/games.vue')
+const stories = require('../../components/stories.vue')
+const freshbeats = require('../../components/freshbeats.vue')
+const contact = require('../../components/contact.vue')
+
+// initialize Vue with above components
 new Vue({
-	el: '#title-screen',
-	data: {
-		title: '',
-		description:''
+	el: 'body',
+	components: {
+		frontends,
+		games,
+		stories,
+		freshbeats,
+		contact
 	}
 });
 
+// init vars for animation
 const nameTime = 1500
 const animationStagger = 300
 const titleLine1 = document.querySelector('#title-screen > *')
@@ -19,13 +32,11 @@ const titleLine2 = document.querySelector('#title-screen > h3')
 // Animate the words in #title-screen individually
 animateCss.animate(titleLine1, {
 	animationName: 'slideInUp',
-	duration: nameTime,
-	stagger:animationStagger
+	duration: nameTime
 })
 
 // Animate the words in "Developer of..." individually while that's happening
 animateCss.animate(titleLine2, {
 	animationName: 'slideInUp',
-	duration: nameTime,
-	stagger:250
+	duration: nameTime
 })

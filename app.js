de0400f4fbeb3c15708faeb13400cb050a3b321e
Noel Quiles <noelquilesgraphics@gmail.com>
2016-06-25 00:06:43 -0400

    VueJS Integration V: The Old-Fashioned Way
    Moving away from single-file components having script for now, as they are what I think is the problem.  Moved the script in frontends.vue into index.js, also reconfigured everything for the latest Spike.  Next time on spike-rework: EnMod Turns The Key!  Stay tuned!

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const sugarss = require('sugarss')
 const lost = require('lost')
 const es2015 = require('babel-preset-es2015')
 const stage2 = require('babel-preset-stage-2')
+const Util = require('spike-util')
+const jade = require('posthtml-jade')
+const md = require('posthtml-md')
+const postcssImport = require('postcss-import')
 
 module.exports = {
 	module: {
@@ -16,18 +20,31 @@ module.exports = {
 			}
 		]
 	},
+	matchers: {
+		html:['**/*.jade','**/*.pug'],
+		css: '**/*.sss'
+	},
 	vue: {
 		postcss:{
-			plugins: [mixins, simpleVars, cssnext, rucksack, lost],
+			plugins: [mixins(), simpleVars(), cssnext(), rucksack(), lost()],
 			options: {
 				parser: sugarss
 			}
 		},
 		autoprefixer: false
 	},
-	postcss: {
-		plugins: [mixins, simpleVars, cssnext, rucksack, lost],
-		parser: sugarss
+	postcss: (ctx) => {
+		const atImport = postcssImport({ addDependencyTo: ctx })
+		return {
+			plugins: [atImport, mixins(), simpleVars(), cssnext(), rucksack(), lost()],
+			parser: sugarss
+		}
+	},
+	posthtml: (ctx) => {
+		const f = Util.filePathFromLoader(ctx).absolute
+		return {
+			defaults: [jade({ filename: f, pretty: true, foo: 'bar' })]
+		}
 	},
 	babel: { presets: [es2015, stage2] },
 	ignore: ['**/layout.jade', '**/_*', '**/.*']

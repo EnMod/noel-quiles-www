6a5bbad779724398c199d5262305794ad652ffd7
Noel Quiles <noelquilesgraphics@gmail.com>
2016-07-15 15:51:04 -0400

    Finally back to basics!  `tnt-carousel` incoming

--- app.js
+++ app.js
@@ -4,13 +4,31 @@ const cssnext = require('postcss-cssnext')
 const rucksack = require('rucksack-css')
 const sugarss = require('sugarss')
 const lost = require('lost')
+const es2015 = require('babel-preset-es2015')
+const stage2 = require('babel-preset-stage-2')
+const Util = require('spike-util')
+const jade = require('posthtml-jade')
+const md = require('posthtml-md')
+const postcssImport = require('postcss-import')
 
 module.exports = {
-  postcss: {
-    plugins: [mixins, simpleVars, cssnext, rucksack, lost],
-    parser: sugarss
-  },
-  babelConfig: { presets: ['es2015', 'stage-2'] },
-  locals: { foo: 'bar' },
-  ignore: ['**/layout.jade', '**/_*', '**/.*']
+	matchers: {
+		html:'**/*.jade',
+		css: '**/*.sss'
+	},
+	postcss: (ctx) => {
+		const atImport = postcssImport({ addDependencyTo: ctx })
+		return {
+			plugins: [atImport, mixins(), simpleVars(), cssnext(), rucksack(), lost()],
+			parser: sugarss
+		}
+	},
+	posthtml: (ctx) => {
+		const f = Util.filePathFromLoader(ctx).absolute
+		return {
+			defaults: [jade({ filename: f, pretty: true, foo: 'bar' })]
+		}
+	},
+	babel: { presets: [es2015, stage2] },
+	ignore: ['**/layout.jade', '**/_*', '**/.*']
 }

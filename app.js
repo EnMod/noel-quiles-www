f7bc72be60289c9d7ba365ce4a2e81305561872a
EnMod <contact@noelquiles.com>
2016-09-08 00:14:57 -0400

    Update to Spike 0.12
    Includes `app.js` rewrite and migration to `reshape` for the templates

--- app.js
+++ app.js
@@ -1,41 +1,28 @@
 const mixins = require('postcss-mixins')
 const simpleVars = require('postcss-simple-vars')
-const cssnext = require('postcss-cssnext')
-const rucksack = require('rucksack-css')
-const sugarss = require('sugarss')
 const lost = require('lost')
-const es2015 = require('babel-preset-es2015')
-const stage2 = require('babel-preset-stage-2')
-const Util = require('spike-util')
-const jade = require('posthtml-jade')
-const md = require('posthtml-md')
-const retext = require('posthtml-retext')
-const smartypants = require('retext-smartypants')
-const postcssImport = require('postcss-import')
-
+const htmlStandards = require('reshape-standard')
+const cssStandards = require('spike-css-standards')
+const jsStandards = require('babel-preset-latest')
+const pageId = require('spike-page-id')
 module.exports = {
+	devtool: 'source-map',
 	matchers: {
-		html:'**/*.jade',
-		css: '**/*.sss'
+		html: '*(**/)*.sgr',
+		css: '*(**/)*.sss'
+	},
+	ignore: ['**/layout.sgr', '**/_*', '**/.*'],
+	reshape: (ctx) => {
+		return htmlStandards({
+			webpack: ctx,
+			locals: { pageId: pageId(ctx), foo: 'bar' }
+		})
 	},
 	postcss: (ctx) => {
-		const atImport = postcssImport({ addDependencyTo: ctx })
 		return {
-			plugins: [atImport, mixins(), simpleVars(), cssnext(), rucksack(), lost()],
-			parser: sugarss
+			cssStandards({webpack: ctx}),
+			plugins: [mixins(), lost()]
 		}
 	},
-	posthtml: (ctx) => {
-		const f = Util.filePathFromLoader(ctx).absolute
-		return {
-			defaults: [
-				jade({ filename: f, pretty: true, foo: 'bar' }),
-				md(),
-				retext([smartypants])
-			]
-		}
-	},
-	vendor: 'assets/vendor/**',
-	babel: { presets: [es2015, stage2] },
-	ignore: ['**/layout.jade', '**/_*', '**/.*']
+	babel: { presets: [jsStandards] }
 }

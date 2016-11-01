4f7e896b7ff965390a74cd8045b9c671bd15fed8
EnMod <contact@noelquiles.com>
2016-11-01 16:50:01 -0400

    Carousels are old hat! The people want...
    ...Masonry.  And by "the people" I mean me.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,26 @@
 const animateCss = require('animate.css-js')
-const tnt = require('tnt-carousel')
-
+const Masonry = require('masonry-layout')
 const nameTime = 1500
 const animationStagger = 300
 const titleLine1 = document.querySelector('#title-screen > *')
 const titleLine2 = document.querySelector('#title-screen > h3')
 
-// call owlCarousel on the frontends gallery
-new tnt(document.getElementById('frontendscar'), {
-	autoplay: true,
-	autoplayInterval: 5000
-})
+const masonryGrid = document.querySelector('.frontendsGrid')
 
+const msnry = new Masonry(masonryGrid, {
+  itemSelector: '.car-item',
+  columnWidth: 200
+})
 // Animate the words in #title-screen individually
 animateCss.animate(titleLine1, {
-	animationName: 'slideInUp',
-	duration: nameTime,
-	stagger:animationStagger
+  animationName: 'slideInUp',
+  duration: nameTime,
+  stagger: animationStagger
 })
 
 // Animate the words in "Developer of..." individually while that's happening
 animateCss.animate(titleLine2, {
-	animationName: 'slideInUp',
-	duration: nameTime,
-	stagger:250
+  animationName: 'slideInUp',
+  duration: nameTime,
+  stagger: 250
 })

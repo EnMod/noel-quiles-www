730886152c0c3a60fbf119be0a014ac730b95331
EnMod <contact@noelquiles.com>
2016-12-09 17:00:44 -0500

    Go back...back to the beginning...
    ...and remember why you were redesigning this thing in the first place.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,9 @@
 const animateCss = require('animate.css-js')
-const Masonry = require('masonry-layout')
 const nameTime = 1500
 const animationStagger = 300
 const titleLine1 = document.querySelector('#title-screen > *')
 const titleLine2 = document.querySelector('#title-screen > h3')
 
-const masonryGrid = document.querySelector('.frontendsGrid')
-
-const msnry = new Masonry(masonryGrid, {
-  itemSelector: '.car-item',
-  columnWidth: 400
-})
 // Animate the words in #title-screen individually
 animateCss.animate(titleLine1, {
   animationName: 'slideInUp',

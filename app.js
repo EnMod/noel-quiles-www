5816a758a7659075a77a22b7905d11b0e366648e
EnMod <contact@noelquiles.com>
2018-03-02 13:34:04 -0500

    works at base

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cssStandards = require('spike-css-standards')
 const jsStandards = require('spike-js-standards')
 const pageId = require('spike-page-id')
 const dotenv = require('dotenv').config()
+const sugarml = require('sugarml')
+const sugarss = require('sugarss')
 
 const SpikeDatoCMS = require('spike-datocms')
 const locals = {}
@@ -22,14 +24,18 @@ module.exports = {
       json: 'data.json'
     })
   ],
-  // devtool: 'source-map',
   matchers: {
     html: '*(**/)*.sgr',
     css: '*(**/)*.sss'
   },
   vendor: ['assets/twine/**'],
   ignore: ['**/layout.sgr', 'assets/css/_*', 'views/**/_*', '**/.*', 'readme.md', 'yarn.lock'],
-  reshape: htmlStandards({ locals: () => locals }),
-  postcss: cssStandards(),
+  reshape: htmlStandards({ 
+    parser: sugarml,
+    locals: () => locals 
+  }),
+  postcss: cssStandards({
+    parser: sugarss  
+  }),
   babel: jsStandards()
 }

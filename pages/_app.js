5f8b2662339870d597a0ff5750398ea3fb622f7a
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2019-11-29 17:43:41 -0500

    Get a base layout together

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import App from "next/app";
+import BaseLayout from "../layouts/BaseLayout.jsx";
+
+export default class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <BaseLayout>
+        <Component {...pageProps} />
+      </BaseLayout>
+    );
+  }
+}

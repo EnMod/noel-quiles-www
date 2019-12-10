55b79c00b0462dfc45cfd1c919e89c38db698880
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2019-12-10 01:08:03 -0500

    Style it up!
    - Prep site to use the Google fonts Questrial and Lustria
    - Add some wide-coverage postcss config via postcss-preset-env
    - Fix the postcss imports by properly using the right package for them

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <div id="p-home">
       <h1 className="headline">I'm Noel Quiles, and</h1>
-      <div className="manifesto">I connect to create.</div>
+      <div className="manifesto g-text-hero">I connect to create.</div>
     </div>
   )
 }
--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   plugins: {
-    "postcss-nesting": {}
+    "postcss-import": {},
+    "postcss-preset-env": {
+      stage: 0,
+      features: {
+        "custom-properties": { preserve: false },
+        "nesting-rules": true
+      }
+    }
   }
-};
+}

37498b582e3da1fd471319594646a529191a4185
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2019-12-16 23:18:12 -0500

    Simplifications/efficiency
    - Broaden scope of DisciplineLinks to cover any page-icon-links
    - Simplify data fetching
    - Remove styles/components.css (it's unneeded)
    - Use custom property for margins in HomePage

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,16 +1,21 @@
 import "./style.css"
 import fetchFromCms from "../../lib/cmsClient"
-import DisciplineLink from "../../components/DisciplineLink"
+import PageIconLink from "../../components/PageIconLink"
 import query from "./query.graphql"
 
-function HomePage({ disciplines }) {
+function HomePage({ disciplines, details }) {
   return (
     <div id="p-home">
       <h1 className="headline">I'm Noel Quiles, and</h1>
       <div className="manifesto g-text-hero">I connect to create.</div>
-      <div className="disciplines">
+      <div className="link-row">
         {disciplines.map(discipline => (
-          <DisciplineLink {...discipline} />
+          <PageIconLink key={discipline.title} {...discipline} />
+        ))}
+      </div>
+      <div className="link-row">
+        {details.map(detail => (
+          <PageIconLink key={detail.title} {...detail} />
         ))}
       </div>
     </div>
@@ -18,11 +23,12 @@ function HomePage({ disciplines }) {
 }
 
 export async function unstable_getStaticProps() {
-  const { allDisciplines } = await fetchFromCms(query)
+  const { disciplines, details } = await fetchFromCms(query)
 
   return {
     props: {
-      disciplines: allDisciplines
+      disciplines,
+      details
     }
   }
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,9 @@
 import Home, { unstable_getStaticProps as staticProps } from "./home"
 
-function HomePage(props) {
-  return <Home {...props} />
-}
+Home.layout = "BareLayout"
 
 export async function unstable_getStaticProps() {
   return staticProps()
 }
 
-HomePage.layout = "BareLayout"
-
-export default HomePage
+export default Home

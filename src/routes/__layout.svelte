596bd5ec32b7d6e6a2a456a352560583c235146a
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2021-07-03 22:55:43 -0400

    Restructure routes

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { page } from '$app/stores'
+
+  import Nav from '$lib/components/nav.svelte'
+</script>
+
+<!-- Make exception for homepage  -->
+{#if page.path === '/'}
+  <slot />
+{:else}
+  <div>Header?</div>
+  <Nav />
+  <input type="text" placeholder="test input to test layout persistence!" />
+  <slot />
+  <div>Footer?</div>
+{/if}
--- src/routes/otherpages/__layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import Nav from '$lib/components/nav.svelte'
-</script>
-
-<div>Header?</div>
-<Nav />
-<input type="text" placeholder="test input to test layout persistence!" />
-<slot />
-<div>Footer?</div>

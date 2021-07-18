2def61805b8ac41bf9478f0cb6e3c585212f6499
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2021-07-18 04:02:57 -0400

    UNTESTED: Impl. dev/prod switch for gql

--- site/src/lib/utils/gql-request.js
+++ site/src/lib/utils/gql-request.js
@@ -1,11 +1,12 @@
 import { GraphQLClient, gql } from "graphql-request"
+import {dev} from '$app/env'
+const gqlEndpoint = dev ? import.meta.env.VITE_GQL_ENDPOINT_DEV : import.meta.env.VITE_GQL_ENDPOINT_PROD
 
-let client = new GraphQLClient(import.meta.env.VITE_GQL_ENDPOINT, {
+let client = new GraphQLClient(gqlEndpoint, {
   headers: {
     Authorization: `Bearer ${import.meta.env.VITE_GQL_AUTH}`
   }
 })
-
 
 export default function gqlr(gqlStr, variables) {
   const query = gql`${gqlStr}`

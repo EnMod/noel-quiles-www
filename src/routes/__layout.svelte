6fb9fa7214cb4bdd806e622ce3a0f91b82999257
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2022-01-18 01:04:04 -0500

    Prototyping theming as a test

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,22 +3,68 @@
   //  * @type {import('@sveltejs/kit').Load}
   //  */
   export async function load({ url }) {
-    return { props: { isHome: url.pathname === '/' } }
+    let slug = '/'
+
+    if (url.pathname !== '/') {
+      slug = url.pathname.split('/')[1]
+    }
+    return {
+      props: {
+        isHome: url.pathname === '/',
+        slug
+      }
+    }
   }
 </script>
 
 <script lang="ts">
-  import Nav from '$lib/components/nav.svelte'
+  import Header from '$lib/components/header.svelte'
+  import Footer from '$lib/components/footer.svelte'
 
   export let isHome
+  export let slug
 </script>
 
 <!-- Make exception for homepage  -->
 {#if isHome}
   <main><slot /></main>
 {:else}
-  <Nav />
-  <input type="text" placeholder="test input to test layout persistence!" />
-  <main><slot /></main>
-  <footer>Footer?</footer>
+  <Header />
+  <!-- TODO Change body class instead? Refresh memory on theme toggling at the <body> level 😅 -->
+  <main class={slug}>
+    <slot />
+  </main>
+  <Footer />
 {/if}
+
+<style lang="postcss">
+  main {
+    border-color: var(--qi-9);
+    color: var(--qi-9);
+    background-color: var(--qi-1);
+
+    &.websites {
+      border-color: var(--water-9);
+      color: var(--water-9);
+      background-color: var(--water-2);
+    }
+
+    &.writing {
+      border-color: var(--earth-9);
+      color: var(--earth-9);
+      background-color: var(--earth-2);
+    }
+
+    &.audio {
+      border-color: var(--wind-9);
+      color: var(--wind-9);
+      background-color: var(--wind-2);
+    }
+
+    &.games {
+      border-color: var(--fire-9);
+      color: var(--fire-9);
+      background-color: var(--fire-2);
+    }
+  }
+</style>

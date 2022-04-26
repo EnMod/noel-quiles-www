b46c2f92df1e78909af4d84948f13d401889a2b0
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2022-04-25 21:00:26 -0400

    fix: Remove target from config

--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,6 @@ import adapterStatic from '@sveltejs/adapter-static'
 
 const config = {
   kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
     adapter: adapterStatic()
   },
   preprocess: sveltePreprocess({ postcss: true })

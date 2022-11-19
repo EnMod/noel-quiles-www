bf92509a4ac21c2bc8c64f307092cee6889e4b42
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2022-11-19 17:56:48 -0500

    feat: Create and implement BlogImage component

---
+++ src/lib/components/blog/image/index.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  interface BlogImageProps {
+    image: {
+      url: string
+      alt?: string
+    }
+    floatPosition?: 'left' | 'right' | 'center'
+    caption?: string
+  }
+
+  export let image: BlogImageProps['image']
+  export let floatPosition: BlogImageProps['floatPosition'] = 'center'
+  export let caption: BlogImageProps['caption'] = ''
+</script>
+
+<figure class="blog-image {floatPosition}">
+  <img srcset={image.url} alt={image.alt} />
+
+  {#if caption}
+    <figcaption class="caption">{caption}</figcaption>
+  {/if}
+</figure>
+
+<style lang="postcss">
+  .blog-image {
+    margin: 0;
+    display: grid;
+    place-items: center;
+
+    & img {
+      max-width: 100%;
+    }
+
+    &.left {
+      float: left;
+      margin-right: 16px;
+    }
+
+    &.right {
+      float: right;
+      margin-left: 16px;
+    }
+  }
+
+  .caption {
+    composes: label from global;
+    padding-top: 8px;
+    color: var(--qi-5);
+  }
+</style>
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,51 @@
-<div>Blog here</div>
+<script lang="ts">
+  import BlogImage from '$lib/components/blog/image/index.svelte'
+</script>
+
+<div class="blog-index">
+  <h1>Blog here</h1>
+  <BlogImage
+    image={{ url: 'https://placekitten.com/400/400' }}
+    caption="This is a caption boiiii"
+  />
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquid expedita illo?
+    Praesentium alias, rerum saepe sapiente nemo adipisci vel quis maiores dolore soluta, quae
+    corporis magnam, ducimus veniam odit! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Itaque aliquid expedita illo? Praesentium alias, rerum saepe sapiente nemo adipisci vel quis
+    maiores dolore soluta, quae corporis magnam, ducimus veniam odit!Lorem ipsum dolor sit amet
+    consectetur adipisicing elit. Itaque aliquid expedita illo? Praesentium alias, rerum saepe
+    sapiente nemo adipisci vel quis maiores dolore soluta, quae corporis magnam, ducimus veniam
+    odit!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquid expedita illo?
+    Praesentium alias, rerum saepe sapiente nemo adipisci vel quis maiores dolore soluta, quae
+    corporis magnam, ducimus veniam odit! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Itaque aliquid expedita illo? Praesentium alias, rerum saepe sapiente nemo adipisci vel quis
+    maiores dolore soluta, quae corporis magnam, ducimus veniam odit!Lorem ipsum dolor sit amet
+    consectetur adipisicing elit. Itaque aliquid expedita illo? Praesentium alias, rerum saepe
+    sapiente nemo adipisci vel quis maiores dolore soluta, quae corporis magnam, ducimus veniam
+    odit!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquid expedita illo?
+    Praesentium alias, rerum saepe sapiente nemo adipisci vel quis maiores dolore soluta, quae
+    corporis magnam, ducimus veniam odit! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Itaque aliquid expedita illo? Praesentium alias, rerum saepe sapiente nemo adipisci vel quis
+    maiores dolore soluta, quae corporis magnam, ducimus veniam odit!Lorem ipsum dolor sit amet
+    consectetur adipisicing elit. Itaque aliquid expedita illo? Praesentium alias, rerum saepe
+    sapiente nemo adipisci vel quis maiores dolore soluta, quae corporis magnam, ducimus veniam
+    odit!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquid expedita illo?
+    Praesentium alias, rerum saepe sapiente nemo adipisci vel quis maiores dolore soluta, quae
+    corporis magnam, ducimus veniam odit! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Itaque aliquid expedita illo? Praesentium alias, rerum saepe sapiente nemo adipisci vel quis
+    maiores dolore soluta, quae corporis magnam, ducimus veniam odit!Lorem ipsum dolor sit amet
+    consectetur adipisicing elit. Itaque aliquid expedita illo? Praesentium alias, rerum saepe
+    sapiente nemo adipisci vel quis maiores dolore soluta, quae corporis magnam, ducimus veniam
+    odit!
+  </p>
+</div>

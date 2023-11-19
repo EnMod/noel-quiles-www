7d9650e2ecfd01b5d9630d990ed97457470354ab
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2023-11-18 21:27:22 -0500

    refactor/bikeshed: metal -> void
    Void isn't a tangible element, but it's a catch-all by nature and is the
    default color scheme of the site, so I suppose it fits better than
    metal? the default scheme icon is more similar to void anyway 🤷

--- src/routes/blog/[post]/components/image.svelte
+++ src/routes/blog/[post]/components/image.svelte
@@ -1,49 +1,49 @@
 <script lang="ts">
-  interface BlogImageProps {
-    image: {
-      url: string
-      alt?: string
-    }
-    imagePosition?: 'left' | 'right' | 'center'
-    caption?: string
-  }
+	interface BlogImageProps {
+		image: {
+			url: string
+			alt?: string
+		}
+		imagePosition?: 'left' | 'right' | 'center'
+		caption?: string
+	}
 
-  export let image: BlogImageProps['image']
-  export let imagePosition: BlogImageProps['imagePosition'] = 'center'
-  export let caption: BlogImageProps['caption'] = ''
+	export let image: BlogImageProps['image']
+	export let imagePosition: BlogImageProps['imagePosition'] = 'center'
+	export let caption: BlogImageProps['caption'] = ''
 </script>
 
 <figure class="blog-image {imagePosition}">
-  <img srcset={image.url} alt={image.alt} />
+	<img srcset={image.url} alt={image.alt} />
 
-  {#if caption}
-    <figcaption class="caption g-label">{caption}</figcaption>
-  {/if}
+	{#if caption}
+		<figcaption class="caption g-label">{caption}</figcaption>
+	{/if}
 </figure>
 
 <style lang="postcss">
-  .blog-image {
-    margin: 0;
-    display: grid;
-    place-items: center;
+	.blog-image {
+		margin: 0;
+		display: grid;
+		place-items: center;
 
-    & img {
-      max-width: 100%;
-    }
+		& img {
+			max-width: 100%;
+		}
 
-    &.left {
-      float: left;
-      margin-right: var(--sz-05);
-    }
+		&.left {
+			float: left;
+			margin-right: var(--sz-05);
+		}
 
-    &.right {
-      float: right;
-      margin-left: var(--sz-05);
-    }
-  }
+		&.right {
+			float: right;
+			margin-left: var(--sz-05);
+		}
+	}
 
-  .caption {
-    padding-top: var(--sz-04);
-    color: var(--metal-5);
-  }
+	.caption {
+		padding-top: var(--sz-04);
+		color: var(--void-5);
+	}
 </style>

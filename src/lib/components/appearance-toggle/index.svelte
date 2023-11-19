c34eb21b10795e14fc555d81e79608a04d6a55f6
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2023-11-18 19:47:50 -0500

    chore: AppearanceToggle updates
    - Tabbed indentation
    - Actual Map for the icon map
    - CSS var for icon size

--- src/lib/components/appearance-toggle/index.svelte
+++ src/lib/components/appearance-toggle/index.svelte
@@ -1,57 +1,60 @@
 <script lang="ts">
-  import { theme } from '$lib/stores'
-  import { tweened } from 'svelte/motion'
-  import LightIcon from '$lib/img/appearance-toggle/light.svelte'
-  import DarkIcon from '$lib/img/appearance-toggle/dark.svelte'
+	import { theme } from '$lib/stores'
+	import { tweened } from 'svelte/motion'
+	import LightIcon from '$lib/img/appearance-toggle/light.svelte'
+	import DarkIcon from '$lib/img/appearance-toggle/dark.svelte'
+	import type { ComponentType } from 'svelte'
 
-  let iconAppearance = getOppositeAppearance()
+	let iconAppearance = getOppositeAppearance()
 
-  const rotationValue = tweened(0, {
-    duration: 150
-  })
+	const rotationValue = tweened(0, {
+		duration: 150
+	})
 
-  function toggleAppearance() {
-    animateToAppearance(getOppositeAppearance())
-  }
+	function toggleAppearance() {
+		animateToAppearance(getOppositeAppearance())
+	}
 
-  function animateToAppearance(appearance) {
-    rotationValue.set(-90).then(() => {
-      $theme.appearance = appearance
-      iconAppearance = getOppositeAppearance()
-      rotationValue.set(0)
-    })
-  }
+	function animateToAppearance(appearance) {
+		rotationValue.set(-90).then(() => {
+			$theme.appearance = appearance
+			iconAppearance = getOppositeAppearance()
+			rotationValue.set(0)
+		})
+	}
 
-  function getOppositeAppearance() {
-    return $theme.appearance === 'light' ? 'dark' : 'light'
-  }
+	function getOppositeAppearance() {
+		return $theme.appearance === 'light' ? 'dark' : 'light'
+	}
 
-  const APPEARANCE_ICON_MAP = {
-    light: LightIcon,
-    dark: DarkIcon
-  }
+	const APPEARANCE_ICON_MAP = new Map<string, ComponentType>([
+		['light', LightIcon],
+		['dark', DarkIcon]
+	])
 </script>
 
 <button on:click={toggleAppearance}>
-  <div class="icon" style="transform:rotateY({$rotationValue}deg)">
-    <svelte:component this={APPEARANCE_ICON_MAP[iconAppearance]} />
-  </div>
+	<div class="icon" style="transform:rotateY({$rotationValue}deg)">
+		<svelte:component this={APPEARANCE_ICON_MAP.get(iconAppearance)} />
+	</div>
 </button>
 
 <style lang="postcss">
-  button {
-    width: 32px;
-    height: 32px;
-    border: 0;
-    background: none;
-    padding: 0;
-    cursor: pointer;
-    perspective: 1200px;
-  }
+	button {
+		--size: 32px;
 
-  .icon {
-    width: 32px;
-    height: 32px;
-    transform-style: preserve-3d;
-  }
+		width: var(--size);
+		height: var(--size);
+		background: none;
+		border: 0;
+		padding: 0;
+		cursor: pointer;
+		perspective: 1200px;
+	}
+
+	.icon {
+		width: var(--size);
+		height: var(--size);
+		transform-style: preserve-3d;
+	}
 </style>
